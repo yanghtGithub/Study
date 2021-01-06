bd3ab98812fff522884284b9cc4753c1cf3e1166
unknown <yhaitao@aliyun.com>
2021-01-06 16:05:23 +0800

    新建RedisStudy项目

---
+++ RedisStudy/redis-01-jedis/src/main/java/com/yht/TestPing.java
@@ -0,0 +1,10 @@
+package com.yht;
+
+import redis.clients.jedis.Jedis;
+
+public class TestPing {
+    public static void main(String[] args) {
+        // 1、new Jedis对象
+        Jedis jedis = new Jedis();
+    }
+}
